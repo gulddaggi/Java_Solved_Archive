dcae4d54da48b33bae99d964b428cbd4c0d1b4d4
gulddaggi <81425764+gulddaggi@users.noreply.github.com>
2025-01-27 02:39:35 +0900

    브론즈 2

--- problem/src/problem/Problem.java
+++ problem/src/problem/Problem.java
@@ -6,6 +6,24 @@ public class Problem {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		int C = sc.nextInt();
+		
+		int[] arr = new int[10];
+		
+		int val = A * B * C;
+		
+		while (val != 0) {
+			int num = val % 10;
+			++arr[num];
+			val /= 10;
+			System.out.println(val);
+		}
+		
+		for (int i = 0; i < arr.length; i++) {
+			System.out.println(arr[i]);
+		}
 	}
 
 }
