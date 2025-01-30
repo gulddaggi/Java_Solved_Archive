c608c58b568103237f58bc32f0e387b669d94223
gulddaggi <81425764+gulddaggi@users.noreply.github.com>
2025-01-30 22:02:11 +0900

    브론즈2

--- problem/src/problem/Problem.java
+++ problem/src/problem/Problem.java
@@ -1,11 +1,46 @@
 package problem;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Problem {
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-	}
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringBuilder sb = new StringBuilder();
 
+		int N = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken());
+		int L = Integer.parseInt(st.nextToken());
+
+		int ans = 0;
+		for (int i = 0; i < N; i++) {
+			int[] arr = new int[3];
+			int total = 0;
+			boolean test = true;
+			
+			st = new StringTokenizer(br.readLine());
+			
+			for (int j = 0; j < 3; j++) {
+				arr[j] = Integer.parseInt(st.nextToken());
+				total += arr[j];
+				if (arr[j] < L) {
+					test = false;
+				}
+			}
+
+			if (test && total >= K) {
+				ans++;
+				sb.append(arr[0] + " ");
+				sb.append(arr[1] + " ");
+				sb.append(arr[2] + " ");
+			}
+		}
+
+		System.out.println(ans);
+		System.out.println(sb);
+	}
 }
