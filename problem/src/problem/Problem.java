88b2d57864ec581852c35d76b3d29df05644bea2
gulddaggi <81425764+gulddaggi@users.noreply.github.com>
2025-01-30 22:06:46 +0900

    빠른 입력 세팅

--- problem/src/problem/Problem.java
+++ problem/src/problem/Problem.java
@@ -1,11 +1,17 @@
 package problem;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Problem {
-
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) throws IOException {
+		// 빠른 입력 사용.
+//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//		StringTokenizer st = new StringTokenizer(br.readLine());
+//		StringBuilder sb = new StringBuilder();
+//
+//		int N = Integer.parseInt(st.nextToken());
 	}
-
 }
