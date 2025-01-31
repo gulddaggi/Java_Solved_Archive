a4a374ef7eef8844dacd7a6f6eed49c57a7cdf1a
gulddaggi <81425764+gulddaggi@users.noreply.github.com>
2025-02-01 00:10:12 +0900

    브론즈2

--- problem/src/problem/Problem.java
+++ problem/src/problem/Problem.java
@@ -1,17 +1,32 @@
 package problem;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.Scanner;
 
 public class Problem {
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) {
 		// 빠른 입력 사용.
 //		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 //		StringTokenizer st = new StringTokenizer(br.readLine());
 //		StringBuilder sb = new StringBuilder();
 //
 //		int N = Integer.parseInt(st.nextToken());
+		
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		
+		for (int i = 0; i < n; i++) {
+			String str = sc.next();
+			int ans = 0;
+			int combo = 0;
+			for (int j = 0; j < str.length(); j++) {
+				if(str.charAt(j) == 'O') {
+					ans += (++combo);
+				}
+				else {
+					combo = 0;
+				}
+			}
+			System.out.println(ans);
+		}
 	}
 }
