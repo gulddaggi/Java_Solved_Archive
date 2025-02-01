de7860c8cb8053ee77c5a0bfccc2dfbfbf055258
gulddaggi <81425764+gulddaggi@users.noreply.github.com>
2025-02-01 21:51:14 +0900

    실버4

--- problem/src/problem/Problem.java
+++ problem/src/problem/Problem.java
@@ -1,17 +1,48 @@
 package problem;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.Scanner;
 
 public class Problem {
-	public static void main(String[] args) throws IOException {
-		// 빠른 입력 사용.
-//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//		StringTokenizer st = new StringTokenizer(br.readLine());
-//		StringBuilder sb = new StringBuilder();
-//
-//		int N = Integer.parseInt(st.nextToken());
+	public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
+        
+        int[] arr = new int[n];
+        for(int i = 0; i < n; i++)
+            arr[i] = sc.nextInt();
+        
+        int num = sc.nextInt();
+        for(int i = 0; i < num; i++){
+            int s = sc.nextInt();
+            int idx = sc.nextInt();
+            
+            if(s == 1){
+                for(int j = idx-1; j < n; j += idx)
+                    arr[j] = (arr[j] == 1) ? 0 : 1;
+            }
+            else{
+                --idx;
+                arr[idx] = (arr[idx] == 1) ? 0 : 1;
+                
+                int val = 1;
+                while(true){
+                    int left = idx - val;
+                    int right = idx + val;
+                    
+                    if(left < 0 || right >= n || arr[left] != arr[right])
+                        break;
+                    
+                    arr[left] = (arr[left] == 1) ? 0 : 1;
+                    arr[right] = (arr[right] == 1) ? 0 : 1;
+                    ++val;
+                }
+            }
+        }
+        
+        for(int i = 0; i < n; i++){
+            if(i != 0 && i % 20 == 0) System.out.println("");
+            System.out.print(arr[i] + " ");
+        }
 	}
 }
