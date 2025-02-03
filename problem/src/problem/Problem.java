2f7db72afd95cda25e178f3345bb8c824863e2d6
gulddaggi <81425764+gulddaggi@users.noreply.github.com>
2025-02-04 00:08:38 +0900

    브론즈1

--- problem/src/problem/Problem.java
+++ problem/src/problem/Problem.java
@@ -1,17 +1,38 @@
 package problem;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.Scanner;
 
 public class Problem {
-	public static void main(String[] args) throws IOException {
-		// 빠른 입력 사용.
-//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//		StringTokenizer st = new StringTokenizer(br.readLine());
-//		StringBuilder sb = new StringBuilder();
-//
-//		int N = Integer.parseInt(st.nextToken());
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		
+		game:
+		for (int i = 0; i < N; i++) {
+			int[] arrA = new int[4];
+			int[] arrB = new int[4];
+			
+			int countA = sc.nextInt();
+			for (int j = 0; j < countA; j++)
+				++arrA[sc.nextInt()-1];
+			
+			int countB = sc.nextInt();
+			for (int j = 0; j < countB; j++)
+				++arrB[sc.nextInt()-1];
+			
+			for (int j = 3; j >= 0; j--) {
+				if (arrA[j] > arrB[j]) {
+					System.out.println("A");
+					continue game;
+				}
+				else if (arrB[j] > arrA[j]) {
+					System.out.println("B");
+					continue game;
+				}
+			}
+			
+			System.out.println("D");
+		}
 	}
 }
