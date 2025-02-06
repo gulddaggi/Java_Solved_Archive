5e7a7c20ab815e32b5aced783f5aa5b6bf7ec3ff
gulddaggi <gulddaggi@gmail.com>
2025-02-06 17:44:48 +0900

    브론즈1

--- problem/src/problem/Problem.java
+++ problem/src/problem/Problem.java
@@ -1,17 +1,48 @@
 package problem;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.Scanner;
 
 public class Problem {
-	public static void main(String[] args) throws IOException {
-		// 빠른 입력 사용.
-//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//		StringTokenizer st = new StringTokenizer(br.readLine());
-//		StringBuilder sb = new StringBuilder();
-//
-//		int N = Integer.parseInt(st.nextToken());
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int L = sc.nextInt();
+		int N = sc.nextInt();
+		
+		int[] cake = new int[L+1];
+		
+		int maxValIdx = 0;
+		int maxVal = 0;
+		int maxRealCount = 0;
+		int maxRealCountIdx = 0;
+		for (int i = 1; i <= N; i++) {
+			// 번호 입력
+			int left = sc.nextInt();
+			int right = sc.nextInt();
+			
+			// 최대 기댓값 판별
+			int val = right - left + 1;
+			if(val > maxVal) { // 최대 기댓값일시 값 및 방청객번호 갱신
+				maxVal = val;
+				maxValIdx = i;
+			}
+			
+			// 최대 실제값 판별
+			int realCount = 0;
+			for (int j = left; j <= right; j++) {
+				if(cake[j] == 0) {
+					cake[j] = i;
+					++realCount;
+				}
+			}
+			
+			if(realCount > maxRealCount) {
+				maxRealCount = realCount;
+				maxRealCountIdx = i;
+			}
+		}
+		
+		System.out.println(maxValIdx);
+		System.out.println(maxRealCountIdx);
 	}
 }
