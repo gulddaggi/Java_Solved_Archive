0bb560d3b5b48216d5e06f14ab4d86eba5f0484c
gulddaggi <gulddaggi@gmail.com>
2025-02-06 16:05:05 +0900

    브론즈2

--- problem/src/problem/Problem.java
+++ problem/src/problem/Problem.java
@@ -1,17 +1,31 @@
 package problem;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+/*
+ 문제 : 1978(소수 찾기)
+ 시간 : 180ms
+ 풀이 : 입력받은 수 n에 대해 2부터 n까지 약수를 계산. 약수가 n인 경우에만 n이 소수이므로 카운팅
+ */
+
+import java.util.Scanner;
 
 public class Problem {
-	public static void main(String[] args) throws IOException {
-		// 빠른 입력 사용.
-//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//		StringTokenizer st = new StringTokenizer(br.readLine());
-//		StringBuilder sb = new StringBuilder();
-//
-//		int N = Integer.parseInt(st.nextToken());
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int N = sc.nextInt();
+
+		int ans = 0;
+		for (int i = 0; i < N; i++) {
+			int num = sc.nextInt();
+
+			for (int j = 2; j <= num; j++) {
+				if (num % j == 0) {
+					if(j == num) ++ans;
+					else break;
+				}
+			}
+		}
+
+		System.out.println(ans);
 	}
 }
