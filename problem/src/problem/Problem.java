d97fd778238645cb2aa66ba7918a187cfb9ab301
gulddaggi <gulddaggi@gmail.com>
2025-02-06 14:43:21 +0900

    브론즈2

--- problem/src/problem/Problem.java
+++ problem/src/problem/Problem.java
@@ -1,17 +1,30 @@
 package problem;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+/*
+ 문제 : 10809(알파벳 찾기)
+ 시간 : 192ms
+ 풀이 : 알파벳 소문자 개수만큼의 배열에 입력 문자열 각 원소의 인덱스를 저장하고 이를 출력.
+ 문자열 각 문자에 해당하는 배열 인덱스는 (문자의 아스키코드 - 'a'의 아스키 코드)로 계산 가능.
+ */
+
+import java.util.Scanner;
 
 public class Problem {
-	public static void main(String[] args) throws IOException {
-		// 빠른 입력 사용.
-//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//		StringTokenizer st = new StringTokenizer(br.readLine());
-//		StringBuilder sb = new StringBuilder();
-//
-//		int N = Integer.parseInt(st.nextToken());
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int[] alphabets = new int[26];
+		
+		String S = sc.next();
+		
+		for (int i = 0; i < S.length(); i++) {
+			int idx = S.charAt(i) - 'a';
+			
+			if(alphabets[idx] == 0)
+				alphabets[S.charAt(i) - 'a'] = i+1;
+		}
+
+		for (int i = 0; i < alphabets.length; i++)
+			System.out.print(alphabets[i]-1 + " ");
 	}
 }
