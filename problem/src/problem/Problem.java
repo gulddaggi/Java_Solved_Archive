a979f98dc6a24db6bfb7dd5e3be58e17650b7281
gulddaggi <81425764+gulddaggi@users.noreply.github.com>
2025-02-08 20:31:30 +0900

    실버4

--- problem/src/problem/Problem.java
+++ problem/src/problem/Problem.java
@@ -1,17 +1,42 @@
 package problem;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.Scanner;
+import java.util.Stack;
 
 public class Problem {
-	public static void main(String[] args) throws IOException {
-		// 빠른 입력 사용.
-//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//		StringTokenizer st = new StringTokenizer(br.readLine());
-//		StringBuilder sb = new StringBuilder();
-//
-//		int N = Integer.parseInt(st.nextToken());
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		Stack<Integer> st = new Stack<>();
+		
+		for (int i = 0; i < N; i++) {
+			String order = sc.next();
+			
+			char c = order.charAt(0);
+			if (c == 'p') { // push, pop
+				char c2 = order.charAt(1);
+				
+				if (c2 == 'u') { // push
+					int val = sc.nextInt();
+					st.push(val);
+				}
+				else { // pop
+					if(st.empty()) System.out.println(-1);
+					else System.out.println(st.pop());
+				}
+			}
+			else if (c == 't') { // top
+				if(st.empty()) System.out.println(-1);
+				else System.out.println(st.peek());
+			}
+			else if (c == 's') { // size
+				System.out.println(st.size());
+			}
+			else { // empty
+				if(st.empty()) System.out.println(1);
+				else System.out.println(0);
+			}
+		}
 	}
 }
