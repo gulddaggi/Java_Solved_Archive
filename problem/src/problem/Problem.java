a331e7f61a09639b878ed7e86bda04f9d562763e
gulddaggi <81425764+gulddaggi@users.noreply.github.com>
2025-02-08 22:27:33 +0900

    실버4

--- problem/src/problem/Problem.java
+++ problem/src/problem/Problem.java
@@ -1,17 +1,53 @@
 package problem;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+/*
+ 문제 : 10845 (큐)
+ 시간 : 460ms
+ 풀이 : 문제에서 요구하는 명령어 맞는 큐 제공 메서드를 사용.
+ 문제 입력 형식에 맞는 분기 구성 및 큐가 비어있는 상황 고려가 필요.
+ */
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
 
 public class Problem {
-	public static void main(String[] args) throws IOException {
-		// 빠른 입력 사용.
-//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//		StringTokenizer st = new StringTokenizer(br.readLine());
-//		StringBuilder sb = new StringBuilder();
-//
-//		int N = Integer.parseInt(st.nextToken());
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		Queue<Integer> q = new LinkedList<>();
+		
+		int back = 0;
+		for (int i = 0; i < N; i++) {
+			String str = sc.next();
+			char c = str.charAt(0);
+			
+			if (c == 'p') { // push, pop				
+				if (str.charAt(1) == 'u') { // push
+					back = sc.nextInt();
+					q.add(back);
+				}
+				else { // pop
+					if (q.isEmpty()) System.out.println(-1);
+					else System.out.println(q.poll());
+				}
+			}
+			else if (c == 'f') { // front
+				if (q.isEmpty()) System.out.println(-1);
+				else System.out.println(q.peek());
+			}
+			else if (c == 'b') { // back
+				if (q.isEmpty()) System.out.println(-1);
+				else System.out.println(back);
+			}
+			else if (c == 's') { // size
+				System.out.println(q.size());
+			}
+			else { // empty
+				if (q.isEmpty()) System.out.println(1);
+				else System.out.println(0);
+			}
+		}
 	}
 }
