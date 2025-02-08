8a9569e311af8221cd1d45e858789014f03c042f
gulddaggi <81425764+gulddaggi@users.noreply.github.com>
2025-02-09 05:34:20 +0900

    시간초과

--- problem/src/problem/Problem.java
+++ problem/src/problem/Problem.java
@@ -1,17 +1,60 @@
 package problem;
 
+import java.util.Scanner;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Problem {
+	public static int r = 0;
+	public static int c = 0;
+	public static int ans = 0;
+	public static boolean trigger = false;
+	public static int[] dx = {0, 0, 1, 1};
+	public static int[] dy = {0, 1, 0, 1};
+	
+	public static boolean z(int n, int startX, int startY) {
+		if(trigger == true) return true;
+		// Z 모양 탐색을 위해 이동할 방향 배열
+
+		
+		if(n == 1) {
+			for (int i = 0; i < 4; i++) {
+				int nx = startX + dx[i];
+				int ny = startY + dy[i];
+				
+				if(nx == r && ny == c) {
+					trigger = true;
+					return true;
+				}
+				else {
+					ans++;
+				}
+			}
+			
+			return false;
+		}
+		
+		for (int i = 0; i < 4; i++) {
+			if(z(n/2, startX + dx[i] * n, startY + dy[i] * n)) {
+				//System.out.println(n / 2 + " " + (startX + dx[i] * n) + " " + (startY + dy[i] * n));
+				return true;
+			}
+		}
+		
+		return false;
+	}
+	
 	public static void main(String[] args) throws IOException {
-		// 빠른 입력 사용.
-//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//		StringTokenizer st = new StringTokenizer(br.readLine());
-//		StringBuilder sb = new StringBuilder();
-//
-//		int N = Integer.parseInt(st.nextToken());
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		r = Integer.parseInt(st.nextToken()); //행
+		c = Integer.parseInt(st.nextToken()); //열
+		
+		if(z((int)Math.pow(2, N-1), 0, 0)) {
+			System.out.println(ans);
+		}
 	}
 }
