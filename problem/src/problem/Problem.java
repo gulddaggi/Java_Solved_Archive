293db0e6d50cb0cb159919f2efe9b8207f25b504
SSAFY <SSAFY@DESKTOP-3HB59PA>
2025-02-10 12:06:47 +0900

    D1 counting sort

--- problem/src/problem/Problem.java
+++ problem/src/problem/Problem.java
@@ -1,6 +1,5 @@
 package problem;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Problem {
@@ -10,12 +9,22 @@ public class Problem {
 		int N = sc.nextInt();
 		int[] arr = new int[N];
 		
+		int[] counts = new int[101];
+		int[] countingArr = new int[N];
+		
 		for (int i = 0; i < N; i++) {
 			arr[i] = sc.nextInt();
+			++counts[arr[i]];
 		}
 		
-		Arrays.sort(arr);
+		for (int i = 1; i < counts.length; i++) {
+			counts[i] += counts[i-1];
+		}
 		
-		System.out.println(arr[N / 2]);
+		for (int i = N-1; i >= 0; i--) {
+			countingArr[--counts[arr[i]]] = arr[i];
+		}
+		
+		System.out.println(countingArr[N / 2]);
 	}
 }
