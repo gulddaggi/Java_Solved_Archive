b7cb00eeac3e1b4c6bfc23be32923f035c3dc50f
SSAFY <SSAFY@DESKTOP-3HB59PA>
2025-02-10 11:50:43 +0900

    D2

--- problem/src/problem/Problem.java
+++ problem/src/problem/Problem.java
@@ -1,17 +1,32 @@
 package problem;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.Scanner;
 
 public class Problem {
-	public static void main(String[] args) throws IOException {
-		// 빠른 입력 사용.
-//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//		StringTokenizer st = new StringTokenizer(br.readLine());
-//		StringBuilder sb = new StringBuilder();
-//
-//		int N = Integer.parseInt(st.nextToken());
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int T = sc.nextInt();
+		int[] arr = new int[1000];
+		for (int testCase = 1; testCase <= T; testCase++) {
+			int N = sc.nextInt();
+			
+			// 각 점수 등장 횟수 카운팅
+			int[] scores = new int[101];
+			for (int i = 0; i < arr.length; i++) {
+				arr[i] = sc.nextInt();
+				++scores[arr[i]];
+			}
+			
+			// 가장 많이 등장한 점수 도출
+			int ans = 0;
+			for (int i = 0; i < scores.length; i++) {
+				if (scores[ans] <= scores[i]) 
+					ans = i;
+			}
+			
+			
+			System.out.println("#" + N + " " + ans);
+		}
 	}
 }
