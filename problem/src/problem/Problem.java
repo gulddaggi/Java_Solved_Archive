55cc4183f43085729080a0d7192538bc9866bff1
SSAFY <SSAFY@DESKTOP-3HB59PA>
2025-02-10 15:34:50 +0900

    D4

--- problem/src/problem/Problem.java
+++ problem/src/problem/Problem.java
@@ -1,17 +1,45 @@
 package problem;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class Problem {
-	public static void main(String[] args) throws IOException {
-		// 빠른 입력 사용.
-//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//		StringTokenizer st = new StringTokenizer(br.readLine());
-//		StringBuilder sb = new StringBuilder();
-//
-//		int N = Integer.parseInt(st.nextToken());
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		for (int testCase = 1; testCase <= 10; testCase++) {
+			int ans = 1;
+			int N = sc.nextInt();
+			sc.nextLine();
+			
+			List<String[]> list = new ArrayList<String[]>();
+			for (int i = 0; i < N; i++) {
+				String str = sc.nextLine();
+				String[] nums = str.split(" ");
+				list.add(nums);
+			}
+			
+			for (String[] strings : list) {				
+				if (strings.length == 2) { // 리프노드일때
+					if (isOperator(strings[1])) { // 기호일때 연산 불가
+						ans = 0;
+						break;
+					}
+				}
+				else { // 리프노드가 아닐 때
+					if (!isOperator(strings[1])) { // 기호가 아니면 연산 불가
+						ans = 0;
+						break;
+					}					
+				}
+			}
+			
+			System.out.println("#" + testCase + " " + ans);
+		}
+	}
+	
+	static boolean isOperator(String str) {
+		return (str.equals("+") || str.equals("-") || str.equals("*") || str.equals("/"));
 	}
 }
