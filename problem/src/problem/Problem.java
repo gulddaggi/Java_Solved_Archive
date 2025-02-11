3cde27a27150831cb38adf3c648fd2f8f2122501
gulddaggi <81425764+gulddaggi@users.noreply.github.com>
2025-02-12 01:00:02 +0900

    브론즈3

--- problem/src/problem/Problem.java
+++ problem/src/problem/Problem.java
@@ -1,17 +1,20 @@
 package problem;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+/*
+ 문제 : 10824(
+ 시간 : 172ms
+ 풀이 : 한 줄을 입력 받은 뒤 각 split 메서드를 통해 공백 단위로 구분 후, 연속된 두 문자열을 이은 뒤 long으로 형변환하여 합산
+ 입력 범위를 고려하였을 때, int 표현 범위를 벗어날 수 있으므로 long을 사용
+ */
+
+import java.util.Scanner;
 
 public class Problem {
-	public static void main(String[] args) throws IOException {
-		// 빠른 입력 사용.
-//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//		StringTokenizer st = new StringTokenizer(br.readLine());
-//		StringBuilder sb = new StringBuilder();
-//
-//		int N = Integer.parseInt(st.nextToken());
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		String[] nums = sc.nextLine().split(" ");
+		
+		System.out.println(Long.parseLong(nums[0] + nums[1]) + Long.parseLong(nums[2] + nums[3]));
 	}
 }
