e748e8ef01c184e80d40b30b0586027760e31302
SSAFY <SSAFY@DESKTOP-3HB59PA>
2025-02-11 15:41:39 +0900

    D2

--- problem/src/problem/Problem.java
+++ problem/src/problem/Problem.java
@@ -1,17 +1,49 @@
 package problem;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.Scanner;
 
 public class Problem {
-	public static void main(String[] args) throws IOException {
-		// 빠른 입력 사용.
-//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//		StringTokenizer st = new StringTokenizer(br.readLine());
-//		StringBuilder sb = new StringBuilder();
-//
-//		int N = Integer.parseInt(st.nextToken());
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int T = sc.nextInt();
+		
+		for (int testCase = 1; testCase <= T; testCase++) {
+			int N = sc.nextInt();
+			int[][] arr = new int[N][N];
+			
+			int num = 1;
+			int r = 0;
+			int c = -1;
+			int dir = 1;
+			
+			while (true) {
+				for (int i = 0; i < N; i++) { // 수평 이동
+					c += dir;
+					arr[r][c] = num++;
+				}
+				
+				--N;
+				
+				if (N == 0) {
+					break;
+				}
+				
+				for (int i = 0; i < N; i++) { // 수직 이동
+					r += dir;
+					arr[r][c] = num++;
+				}
+				
+				dir *= -1;
+			}
+			
+			System.out.println("#" + testCase);
+			for (int i = 0; i < arr.length; i++) {
+				for (int j = 0; j < arr.length; j++) {
+					System.out.print(arr[i][j] + " ");
+				}
+				System.out.println();
+			}
+		}
 	}
 }
