805b9b8c8b7748c0b2265a15cfcda19d081a3a46
SSAFY <SSAFY@DESKTOP-3HB59PA>
2025-02-11 16:18:16 +0900

    D2

--- problem/src/problem/Problem.java
+++ problem/src/problem/Problem.java
@@ -1,17 +1,47 @@
 package problem;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.Scanner;
 
 public class Problem {
-	public static void main(String[] args) throws IOException {
-		// 빠른 입력 사용.
-//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//		StringTokenizer st = new StringTokenizer(br.readLine());
-//		StringBuilder sb = new StringBuilder();
-//
-//		int N = Integer.parseInt(st.nextToken());
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int[][] arr = new int[100][100];
+		for (int t = 0; t < 10; t++) {
+			int N = sc.nextInt();
+			
+			int ans = 0;
+			
+			for (int i = 0; i < arr.length; i++) {
+				int val = 0;
+				for (int j = 0; j < arr.length; j++) {
+					arr[i][j] = sc.nextInt();
+					
+					val += arr[i][j];
+				}
+				
+				ans = Math.max(ans, val);
+			}
+			
+			for (int i = 0; i < arr.length; i++) {
+				int val = 0;
+				for (int j = 0; j < arr.length; j++) {
+					val += arr[j][i];
+				}
+				
+				ans = Math.max(ans, val);
+			}
+			
+			int valCL = 0;
+			int valCR = 0;
+			for (int i = 0; i < arr.length; i++) {
+				valCL += arr[i][i];
+				valCR += arr[i][arr.length - i - 1];
+			}
+			
+			ans = Math.max(ans, Math.max(valCL, valCR));
+			System.out.println("#" + N + " " + ans);
+		}
+		
 	}
 }
