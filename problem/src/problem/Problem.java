7d45aea9c9b417461bbc18686c0a4c3914bf83fb
SSAFY <SSAFY@DESKTOP-3HB59PA>
2025-02-12 11:50:10 +0900

    D2

--- problem/src/problem/Problem.java
+++ problem/src/problem/Problem.java
@@ -1,17 +1,28 @@
 package problem;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.Scanner;
 
 public class Problem {
-	public static void main(String[] args) throws IOException {
-		// 빠른 입력 사용.
-//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//		StringTokenizer st = new StringTokenizer(br.readLine());
-//		StringBuilder sb = new StringBuilder();
-//
-//		int N = Integer.parseInt(st.nextToken());
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int T = sc.nextInt();
+		
+		for (int testCase = 1; testCase <= T; testCase++) {
+			String str = sc.next();
+			
+			int left = 0;
+			int right = str.length() - 1;
+			
+			int ans = 1;
+			while (left < right) {
+				if (str.charAt(left++) != str.charAt(right--)) {
+                    ans = 0;
+                    break;
+                }				
+			}
+			
+			System.out.println("#" + testCase + " " + ans);
+		}
 	}
 }
