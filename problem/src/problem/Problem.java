6c428309c032034e267714f0259a4b0208f612cf
gulddaggi <81425764+gulddaggi@users.noreply.github.com>
2025-02-12 21:43:34 +0900

    골드5

--- problem/src/problem/Problem.java
+++ problem/src/problem/Problem.java
@@ -1,20 +1,35 @@
 package problem;
 
+/*
+ 문제 : 2470(두 용액)
+ 시간 : 352ms
+ 풀이 : 정렬된 배열의 양 끝 두 지점에서부터 두 용액의 부호 동일 여부와 합의 부호에 따라 각 인덱스를 이동하며 두 용액 값의 합 최소값을 도출.
+ */
+
 import java.util.Arrays;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
 
 public class Problem {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) throws IOException {
+		// 빠른 입출력
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
 		
-		int N = sc.nextInt();
+		int N = Integer.parseInt(st.nextToken());
 		
 		int[] arr = new int[N];
 		
-		for (int i = 0; i < arr.length; i++) {
-			arr[i] = sc.nextInt();
-		}
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < arr.length; i++)
+			arr[i] = Integer.parseInt(st.nextToken());
 		
+		// 이분 탐색을 위해 배열 정렬
 		Arrays.sort(arr);
 		
 		int val = 2000000001;
@@ -24,48 +39,42 @@ public class Problem {
 		int right = arr.length - 1;
 		
 		while (left < right) {
-			int mid = left + (right - left) / 2;
-			int sum = Math.abs(arr[left] + arr[right]);
-			System.out.println(arr[left] + " / " + arr[right]);
+			int sum = arr[left] + arr[right];
 			
 			// 기준 값보다 작다면 기준 값 갱신
-			if (sum <= val) {
+			if (Math.abs(sum) <= val) {
 				leftVal = arr[left];
 				rightVal = arr[right];
 				
-				if (sum == 0) {
+				if (sum == 0) 
 					break;
-				}
 				
-				val = sum;
+				val = Math.abs(sum);
 			}
 			
-			// 좀 더 작은 합 도출해보기
-			// 부호가 달라지면 더 작아질 가능성 없음
-			if (arr[right] / Math.abs(arr[right]) != arr[mid] / Math.abs(arr[mid])) {
-				left = mid;
+			// 루프 탈출 조건
+			if (right - left == 1) break;
+			
+			// 양쪽 값의 부호가 다를 경우
+			if (arr[right] > 0 && arr[left] < 0) {
+				// 합이 음수라면 더 작은 음수로 이동
+				if (sum < 0) ++left;
+				// 합이 양수라면 더 작은 양수로 이동
+				else --right;
 			}
-			// 부호가 같으면 더 작아질 가능성 있음
+			// 양쪽 값의 부호가 같을 경우(=양쪽 범위 내 동일 부호만 존재). 이분 탐색 수행
 			else {
-				right = mid;
+				int mid = (left + right) / 2;
+				
+				// 음수일 경우 더 작은 음수로 이동
+				if (sum < 0) left = mid;
+				else right = mid;
 			}
-			
-//			// 부호가 달라지면 더 작아질 가능성 없음
-//			if (arr[right] / Math.abs(arr[right]) != arr[right - 1] / Math.abs(arr[right - 1])) {
-//				++left;
-//				right = arr.length - 1;
-//			}
-//			// 부호가 같으면 더 작아질 가능성 있음
-//			else {
-//				--right;
-//			}
-//			
-//			else if (sum > val) {
-//				++left;
-//				right = arr.length - 1;
-//			}
 		}
 		
-		System.out.println(leftVal + " " + rightVal);
+		String ans = leftVal + " " + rightVal;
+		bw.write(ans);
+		bw.flush();
+		bw.close();
 	}
 }
