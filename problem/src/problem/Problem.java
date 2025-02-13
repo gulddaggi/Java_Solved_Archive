995ff8026d5eb313ea292599aa90e7f050a6d2d2
HanJaeSung <81425764+gulddaggi@users.noreply.github.com>
2025-02-13 16:59:38 +0900

    브론즈1

--- problem/src/problem/Problem.java
+++ problem/src/problem/Problem.java
@@ -1,17 +1,47 @@
 package problem;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.Scanner;
 
 public class Problem {
-	public static void main(String[] args) throws IOException {
-		// 빠른 입력 사용.
-//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//		StringTokenizer st = new StringTokenizer(br.readLine());
-//		StringBuilder sb = new StringBuilder();
-//
-//		int N = Integer.parseInt(st.nextToken());
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		String str = sc.next();
+		
+		int holder = 0;
+		boolean couple = false;
+		for (int i = 0; i < N; i++) {
+			char c = str.charAt(i);
+			
+			if (c == 'S') {
+				if (i == N-1) {
+					holder += 2;
+				}
+				else {
+					++holder;
+				}
+			}
+			else {
+				if (couple) {
+					couple = false;
+					
+					if (i == N-1) {
+						++holder;
+					}
+				}
+				else {
+					couple = true;
+					++holder;
+				}
+			}
+		}
+		
+		if (holder >= N) {
+			System.out.println(N);
+		}
+		else {
+			System.out.println(holder);
+		}
 	}
 }
