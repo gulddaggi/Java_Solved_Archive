c236589370dbed6d7fe245dcdb125177ec6faabe
HanJaeSung <81425764+gulddaggi@users.noreply.github.com>
2025-02-13 14:31:46 +0900

    D4

--- problem/src/problem/Problem.java
+++ problem/src/problem/Problem.java
@@ -1,17 +1,54 @@
 package problem;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.Scanner;
+import java.util.Stack;
 
 public class Problem {
-	public static void main(String[] args) throws IOException {
-		// 빠른 입력 사용.
-//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//		StringTokenizer st = new StringTokenizer(br.readLine());
-//		StringBuilder sb = new StringBuilder();
-//
-//		int N = Integer.parseInt(st.nextToken());
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		for (int testCase = 1; testCase <= 10; testCase++) {
+			Stack<Character> st = new Stack<>();
+			
+			int num = sc.nextInt();
+			String str = sc.next();
+			
+			int ans = 1;
+			for (int i = 0; i < num; i++) {
+				char c = str.charAt(i);
+				
+				// 여는 괄호 입력
+				if (c == '(' || c == '[' || c == '{' || c == '<') {
+					st.push(c);
+				}
+				// 닫는 괄호 입력
+				else {
+					// 스택이 비어있는 경우 문자열 유효하지 않음
+					if (st.empty()) {
+						ans = 0;
+					}
+					
+					// 각 괄호에 일치하는 여는 괄호가 스택 top에 위치하지 않을 경우 문자열 유효하지 않음 
+					if (c == ')') {
+						if (st.pop() != '(') ans = 0;
+						
+					}
+					else if (c == ']') {
+						if (st.pop() != '[') ans = 0;
+					}
+					else if (c == '}') {
+						if (st.pop() != '{') ans = 0;
+					}
+					else {
+						if (st.pop() != '<') ans = 0;
+					}
+				}
+				
+				if (ans == 0) break;
+			}
+			
+			System.out.println("#" + testCase + " " + ans);
+			
+		}
 	}
 }
