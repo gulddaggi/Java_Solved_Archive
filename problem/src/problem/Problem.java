f7c68ed483dc1538760ee147054eff8cead6e0a8
gulddaggi <81425764+gulddaggi@users.noreply.github.com>
2025-02-14 11:38:54 +0900

    D3

--- problem/src/problem/Problem.java
+++ problem/src/problem/Problem.java
@@ -1,17 +1,28 @@
 package problem;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.Scanner;
 
 public class Problem {
-	public static void main(String[] args) throws IOException {
-		// 빠른 입력 사용.
-//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//		StringTokenizer st = new StringTokenizer(br.readLine());
-//		StringBuilder sb = new StringBuilder();
-//
-//		int N = Integer.parseInt(st.nextToken());
+	static int func(int num, int powNum) {
+		if (powNum == 1) {
+			return num;
+		}
+		
+		return num * func(num, powNum-1);
+	}
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		for (int tc = 0; tc < 10; tc++) {
+			int T = sc.nextInt();
+			
+			int num = sc.nextInt();
+			int powNum = sc.nextInt();
+			
+			System.out.println("#" + T + " " + func(num, powNum));
+			
+		}
+
 	}
 }
