f4cca40e990e28383bf16f9d5e8fe57edb9d500f
HanJaeSung <81425764+gulddaggi@users.noreply.github.com>
2025-02-17 11:44:55 +0900

    실버1

--- problem/src/problem/Problem.java
+++ problem/src/problem/Problem.java
@@ -1,17 +1,61 @@
 package problem;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+class point{
+	int x;
+	int y;
+	
+	point(int x, int y){
+		this.x = x;
+		this.y = y;
+	}
+}
 
 public class Problem {
-	public static void main(String[] args) throws IOException {
-		// 빠른 입력 사용.
-//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//		StringTokenizer st = new StringTokenizer(br.readLine());
-//		StringBuilder sb = new StringBuilder();
-//
-//		int N = Integer.parseInt(st.nextToken());
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int[] dx = {0, -1, 0, 1};
+		int[] dy = {1, 0, -1, 0};
+		
+		int N = sc.nextInt();
+		int M = sc.nextInt();
+		
+		int[][] arr = new int[N][M];
+		for (int i = 0; i < arr.length; i++) {
+			String str = sc.next();
+			
+			for (int j = 0; j < arr[i].length; j++) {
+				arr[i][j] = str.charAt(j) - '0';
+			}
+		}
+		
+		Queue<point> q = new LinkedList<>();
+		point start = new point(0, 0);
+
+		q.add(start);
+		
+		while (!q.isEmpty()) {
+			point cur = q.poll();
+			
+			if (cur.x == N-1 && cur.y == M-1) {
+				System.out.println(arr[cur.x][cur.y]);
+				break;
+			}
+			
+			for (int dir = 0; dir < 4; dir++) {
+				int nx = cur.x + dx[dir];
+				int ny = cur.y + dy[dir];
+				
+				if (nx < 0 || nx >= N || ny < 0 || ny >= M) continue;
+				if (arr[nx][ny] != 1) continue;
+				
+				q.add(new point(nx, ny));
+				arr[nx][ny] = arr[cur.x][cur.y] + 1;
+			}
+		}
 	}
 }
