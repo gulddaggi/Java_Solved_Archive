420ae6d0fcabe416b7817f690ce901a8b4a53164
HanJaeSung <81425764+gulddaggi@users.noreply.github.com>
2025-02-17 15:42:55 +0900

    실버3

--- problem/src/problem/Problem.java
+++ problem/src/problem/Problem.java
@@ -1,17 +1,29 @@
 package problem;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.Scanner;
 
 public class Problem {
-	public static void main(String[] args) throws IOException {
-		// 빠른 입력 사용.
-//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//		StringTokenizer st = new StringTokenizer(br.readLine());
-//		StringBuilder sb = new StringBuilder();
-//
-//		int N = Integer.parseInt(st.nextToken());
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		int[] arr = new int[n+1];
+		
+		for (int i = 1; i <= n; i++) {
+			arr[i] = sc.nextInt();
+		}
+		
+		int[][] dp = new int[n+1][2];
+		
+		
+		dp[1][0] = arr[1];
+		dp[1][1] = 0;
+		
+		for (int i = 2; i <= n; i++) {
+			dp[i][0] = Math.max(dp[i-2][0] + arr[i], dp[i-2][1] + arr[i]);
+			dp[i][1] = dp[i-1][0] + arr[i];
+		}
+		
+		System.out.println(Math.max(dp[n][0], dp[n][1]));
 	}
 }
