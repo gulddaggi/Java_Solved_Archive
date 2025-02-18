05d5c6455eda2c8736caa688d76fef095dc5fd30
HanJaeSung <81425764+gulddaggi@users.noreply.github.com>
2025-02-18 17:56:48 +0900

    구현중

--- problem/src/problem/Problem.java
+++ problem/src/problem/Problem.java
@@ -1,17 +1,17 @@
 package problem;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.Scanner;
 
 public class Problem {
-	public static void main(String[] args) throws IOException {
-		// 빠른 입력 사용.
-//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//		StringTokenizer st = new StringTokenizer(br.readLine());
-//		StringBuilder sb = new StringBuilder();
-//
-//		int N = Integer.parseInt(st.nextToken());
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		String[] nums = sc.next().split(":");
+		
+		int n = Integer.parseInt(nums[0]);
+		int m = Integer.parseInt(nums[1]);
+		
+		
+
 	}
 }
