a7356be4baf65d35faa7dba135eb686575af62d0
HanJaeSung <81425764+gulddaggi@users.noreply.github.com>
2025-02-18 17:33:52 +0900

    브론즈2

--- problem/src/problem/Problem.java
+++ problem/src/problem/Problem.java
@@ -1,17 +1,18 @@
 package problem;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.Scanner;
 
 public class Problem {
-	public static void main(String[] args) throws IOException {
-		// 빠른 입력 사용.
-//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//		StringTokenizer st = new StringTokenizer(br.readLine());
-//		StringBuilder sb = new StringBuilder();
-//
-//		int N = Integer.parseInt(st.nextToken());
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int P = 100 - sc.nextInt();
+		
+		int[] coins = {25, 10, 5, 1};
+		
+		for (int i = 0; i < coins.length; i++) {
+			System.out.print(P / coins[i] + " ");
+			P %= coins[i];
+		}
 	}
 }
