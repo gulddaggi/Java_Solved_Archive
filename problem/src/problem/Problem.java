09ceea62119cb9593166f579181bc664453828aa
gulddaggi <81425764+gulddaggi@users.noreply.github.com>
2025-02-19 00:23:18 +0900

    실버5

--- problem/src/problem/Problem.java
+++ problem/src/problem/Problem.java
@@ -11,7 +11,22 @@ public class Problem {
 		int n = Integer.parseInt(nums[0]);
 		int m = Integer.parseInt(nums[1]);
 		
+		int a, b;
+		if (m > n) {
+			a = m;
+			b = n;
+		}
+		else {
+			a = n;
+			b = m;
+		}
 		
-
+		while (b != 0) {
+			int tmp = b;
+			b = a % b;
+			a = tmp;
+		}
+		
+		System.out.println(n / a + ":" + m / a);
 	}
 }
