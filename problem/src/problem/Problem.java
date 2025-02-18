d06bf1941a382d4f59e94b0efb89385f6c197853
HanJaeSung <81425764+gulddaggi@users.noreply.github.com>
2025-02-18 15:36:56 +0900

    D3

--- problem/src/problem/Problem.java
+++ problem/src/problem/Problem.java
@@ -1,17 +1,55 @@
 package problem;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
 
 public class Problem {
-	public static void main(String[] args) throws IOException {
-		// 빠른 입력 사용.
-//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//		StringTokenizer st = new StringTokenizer(br.readLine());
-//		StringBuilder sb = new StringBuilder();
-//
-//		int N = Integer.parseInt(st.nextToken());
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		for (int testCase = 1; testCase <= 10; testCase++) {
+			int N = sc.nextInt();
+			
+			Queue<String> password = new LinkedList<>();
+			
+			for (int i = 0; i < N; i++) {
+				String str = sc.next();
+				
+				password.add(str);
+			}
+			
+			N = sc.nextInt();
+			for (int i = 0; i < N; i++) {
+				String order = sc.next();
+				
+				int x = sc.nextInt();
+				int y = sc.nextInt();
+				
+				Queue<String> temp = new LinkedList<>();
+				
+				while (x-- != 0) {
+					temp.add(password.poll());
+				}
+				
+				for (int j = 0; j < y; j++) {
+					String addStr = sc.next();
+					
+					temp.add(addStr);
+				}
+				
+				while (!password.isEmpty()) {
+					temp.add(password.poll());
+				}
+				
+				password = temp;
+			}
+			
+			System.out.print("#" + testCase + " ");
+			for (int i = 0; i < 10; i++) {
+				System.out.print(password.poll() + " ");
+			}
+			System.out.println();
+		}
 	}
 }
