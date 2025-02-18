20b9219cf1714f6713676e3da9a60c421d61a9a1
HanJaeSung <81425764+gulddaggi@users.noreply.github.com>
2025-02-18 15:19:25 +0900

    D3

--- problem/src/problem/Problem.java
+++ problem/src/problem/Problem.java
@@ -1,17 +1,49 @@
 package problem;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.Scanner;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class Problem {
-	public static void main(String[] args) throws IOException {
-		// 빠른 입력 사용.
-//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//		StringTokenizer st = new StringTokenizer(br.readLine());
-//		StringBuilder sb = new StringBuilder();
-//
-//		int N = Integer.parseInt(st.nextToken());
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		for (int i = 1; i <= 10; i++) {
+			int testCase = sc.nextInt();
+			
+			Queue<Integer> q = new LinkedList<>();
+			
+			for (int j = 0; j < 8; j++) {
+				int num = sc.nextInt();
+				
+				q.add(num);
+			}
+			
+			int val = 1;
+			while (true) {
+				int cur = q.poll();
+								
+				cur -= val;
+				if (cur <= 0) {
+					cur = 0;
+					q.add(cur);
+					break;
+				}
+				
+				q.add(cur);
+				++val;
+				
+				if (val == 6) {
+					val = 1;
+				}
+			}
+			
+			System.out.print("#" + testCase + " ");
+			while (!q.isEmpty()) {
+				System.out.print(q.poll() + " ");
+			}
+			
+			System.out.println();
+		}
 	}
 }
