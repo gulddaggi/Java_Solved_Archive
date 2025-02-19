87c0bd53b9be79099d26454189a345b4cb499e1f
HanJaeSung <81425764+gulddaggi@users.noreply.github.com>
2025-02-19 13:19:40 +0900

    D3

--- problem/src/problem/Problem.java
+++ problem/src/problem/Problem.java
@@ -1,17 +1,37 @@
 package problem;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.Scanner;
 
 public class Problem {
-	public static void main(String[] args) throws IOException {
-		// 빠른 입력 사용.
-//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//		StringTokenizer st = new StringTokenizer(br.readLine());
-//		StringBuilder sb = new StringBuilder();
-//
-//		int N = Integer.parseInt(st.nextToken());
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int T = sc.nextInt();
+		for (int testCase = 1; testCase <= T; testCase++) {
+			int N = sc.nextInt();
+			
+			int mid = N / 2;
+			int val = 0;
+			int fac = 1;
+			
+			int ans = 0;
+			for (int i = 0; i < N; i++) {
+				String str = sc.next();
+				
+				for (int j = 0; j < str.length(); j++) {
+					if (j >= mid - val && j <= mid + val) {
+						ans += (str.charAt(j) - '0');
+					}
+				}
+				
+				if (val == mid) {
+					fac = -1;
+				}
+				
+				val += fac;
+			}
+			
+			System.out.println("#" + testCase + " " + ans);
+		}
 	}
 }
