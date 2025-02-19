e11bd52f8776dccf68653b1e4168c75fa562f662
HanJaeSung <81425764+gulddaggi@users.noreply.github.com>
2025-02-19 12:59:56 +0900

    D3

--- problem/src/problem/Problem.java
+++ problem/src/problem/Problem.java
@@ -1,17 +1,55 @@
 package problem;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.LinkedList;
+import java.util.Scanner;
 
 public class Problem {
-	public static void main(String[] args) throws IOException {
-		// 빠른 입력 사용.
-//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//		StringTokenizer st = new StringTokenizer(br.readLine());
-//		StringBuilder sb = new StringBuilder();
-//
-//		int N = Integer.parseInt(st.nextToken());
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		for (int testCase = 1; testCase <= 10; testCase++) {
+			int N = sc.nextInt();
+			LinkedList<Integer> list = new LinkedList<>();
+			
+			for (int i = 0; i < N; i++) {
+				list.addLast(sc.nextInt());
+			}
+			
+			int M = sc.nextInt();
+			
+			while (M-- != 0) {
+				String order = sc.next();
+				
+				if (order.equals("I")) {
+					int x = sc.nextInt();
+					int y = sc.nextInt();
+					
+					for (int i = 0; i < y; i++) {
+						list.add(i+x, sc.nextInt());
+					}
+				}
+				else if (order.equals("D")){
+					int x = sc.nextInt();
+					int y = sc.nextInt();
+					
+					for (int i = 0; i < y; i++) {
+						list.remove(x);
+					}
+				}
+				else {
+					int y = sc.nextInt();
+					
+					for (int i = 0; i < y; i++) {
+						list.addLast(sc.nextInt());
+					}
+				}
+			}
+			
+			System.out.print("#" + testCase + " ");
+			for (int i = 0; i < 10; i++) {
+				System.out.print(list.pollFirst() + " ");
+			}
+			System.out.println();
+		}
 	}
 }
