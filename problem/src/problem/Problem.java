841f0e302492825af1cfd967185f8338e8c74efa
HanJaeSung <81425764+gulddaggi@users.noreply.github.com>
2025-02-21 14:29:51 +0900

    D3 클래스 구현

--- problem/src/problem/Problem.java
+++ problem/src/problem/Problem.java
@@ -1,7 +1,61 @@
 package problem;
 
-import java.util.PriorityQueue;
 import java.util.Scanner;
+
+class heap{
+	int[] heap = new int[1000001];
+	int heapSize;
+	
+	heap() {
+		heapSize = 0;
+	}
+	
+	public void push(int val) {
+		heap[++heapSize] = val;
+		
+		int ch = heapSize;
+		int p = ch / 2;
+		
+		while (p > 0 && heap[p] < heap[ch]) {
+			int tmp = heap[p];
+			heap[p] = heap[ch];
+			heap[ch] = tmp;
+			
+			ch = p;
+			p = ch / 2;
+		}
+	}
+	
+	public int pop() {
+		if (heapSize == 0) {
+			return -1;
+		}
+		
+		int val = heap[1];
+		
+		heap[1] = heap[heapSize--];
+		
+		int p = 1;
+		int ch = p * 2;
+		
+		if (ch+1 <= heapSize && heap[ch] < heap[ch+1]) ++ch;
+		
+		while (ch <= heapSize && heap[p] < heap[ch]) {
+			int tmp = heap[p];
+			heap[p] = heap[ch];
+			heap[ch] = tmp;
+			
+			p = ch;
+			ch *= 2;
+			
+			if (ch+1 <= heapSize && heap[ch] < heap[ch+1]) ++ch;
+		}
+		
+		
+		return val;
+	}
+	
+}
 
 public class Problem {
 	public static void main(String[] args) {
@@ -13,9 +67,7 @@ public class Problem {
 			
 			System.out.print("#" + testCase + " ");
 			
-			PriorityQueue<Integer> pq = new PriorityQueue<>((o1, o2)->{
-				return o2 - o1;
-			});
+			heap h = new heap();
 			
 			for (int i = 0; i < N; i++) {
 				int order = sc.nextInt();
@@ -23,13 +75,11 @@ public class Problem {
 				// 삽입 연산
 				if (order == 1) {
 					int num = sc.nextInt();
-					pq.add(num);
+					h.push(num);
 				}
 				// 삭제 연산
 				else {
-					int num = pq.isEmpty() ? -1 : pq.poll();
-
-					System.out.print(num + " ");
+					System.out.print( h.pop() + " ");
 				}
 			}
 			
