2b998c9ec518e52e7fd5cbd19281f9944dc42707
HanJaeSung <81425764+gulddaggi@users.noreply.github.com>
2025-02-21 14:41:08 +0900

    실버2(빠른입출력)

--- problem/src/problem/Problem.java
+++ problem/src/problem/Problem.java
@@ -1,15 +1,21 @@
 package problem;
 
 /*
- 문제 : 11725(트리의 부모 찾기)
- 시간 : 1860ms
- 풀이 : 트리의 루트 노드 1번에서부터 dfs를 수행하며 부모 노드를 갱신.
- 현재 방문 노드가 아직 방문하지 않은 인접 노드를 순회할 때 해당 노드의 부모 노드를 현재 방문 노드임을 이용.
- */
+문제 : 11725(트리의 부모 찾기)
+시간 : 612ms(빠른 입출력 사용. Scanner 사용 시 1860ms)
+풀이 : 트리의 루트 노드 1번에서부터 dfs를 수행하며 부모 노드를 갱신.
+현재 방문 노드가 아직 방문하지 않은 인접 노드를 순회할 때 해당 노드의 부모 노드를 현재 방문 노드임을 이용.
+*/
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Problem {
 	// 인접 리스트로 트리 입력
@@ -19,10 +25,14 @@ public class Problem {
 	// 방문 여부 체크 배열
 	static boolean isVisit[];
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		Scanner sc = new Scanner(System.in);
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringBuilder sb = new StringBuilder();
 		
-		int N = sc.nextInt();
+		int N = Integer.parseInt(st.nextToken());
 		
 		ans = new int[N+1];
 		isVisit = new boolean[N+1];
@@ -34,20 +44,25 @@ public class Problem {
 		
 		// 트리 정보 입력
 		for (int i = 0; i < N-1; i++) {
-			int st = sc.nextInt();
-			int en = sc.nextInt();
+			st = new StringTokenizer(br.readLine());
+			int v1 = Integer.parseInt(st.nextToken());
+			int v2 = Integer.parseInt(st.nextToken());
 			
 			// dfs 수행을 위해 양방향 입력
-			tree.get(st).add(en);
-			tree.get(en).add(st);
+			tree.get(v1).add(v2);
+			tree.get(v2).add(v1);
 		}
 		
 		// 루트에서부터 dfs 수행하며 부모 노드 파악
 		dfs(1);
 		
 		for (int i = 2; i <= N; i++) {
-			System.out.println(ans[i]);
+			sb.append(ans[i] + "\n");
 		}
+		
+		bw.write(sb.toString());
+		bw.flush();
+		bw.close();
 	}
 	
 	static void dfs(int v) {
