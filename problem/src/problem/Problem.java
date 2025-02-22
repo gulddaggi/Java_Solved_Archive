322d1f48ce05fd9dffedd27b67dc2951507a7ecf
gulddaggi <81425764+gulddaggi@users.noreply.github.com>
2025-02-23 00:50:56 +0900

    골드3

--- problem/src/problem/Problem.java
+++ problem/src/problem/Problem.java
@@ -1,5 +1,12 @@
 package problem;
 
+/*
+ 문제 : 17471(게리맨더링)
+ 시간 : 204ms
+ 풀이 : 백트래킹으로 각 구역을 두 그룹으로 나누는 모든 경우의 수를 확인하며 최소값 갱신
+ 두 그룹이 모두 존재할 때, bfs를 통해 각 그룹의 구역에 모두 접근 가능한지 체크.
+ */
+
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
@@ -48,25 +55,30 @@ public class Problem {
 		}
 	}
 	
+	// 백트래킹으로 그룹 분할
 	static void divide(int idx) {
+		// 그룹 분할 완료 후 체크
 		if (idx > N) {
 			List<Integer> groupA = new ArrayList<>();
 			List<Integer> groupB = new ArrayList<>();
 			
+			// 그룹 체크 여부에 따라 그룹 분할
 			for (int i = 1; i <= N; i++) {
 				if (selected[i]) groupA.add(i);
 				else groupB.add(i);
 			}
 			
-			// 두 그룹 모두 하나 이상 포함
+			// 가능한 경우인지 체크
+			// 1. 두 그룹 모두 하나 이상 포함
 			if (!groupA.isEmpty() && !groupB.isEmpty()) {
-				// 두 그룹이 모두 접근 가능한가
+				// 2. 두 그룹이 모두 접근 가능한가
 				if (isConnected(groupA, true) && isConnected(groupB, false)) {
 					int popA = 0;
 					for (Integer num : groupA) popA += popularity[num];
 					
 					int popB = total - popA;
 					
+					// 모두 성립하는 경우 최소값 갱신
 					ans = Math.min(ans, Math.abs(popA - popB));
 				}
 			}
@@ -81,7 +93,9 @@ public class Problem {
 		divide(idx + 1);
 	}
 	
+	// 해당 그룹에 속한 노드로 모두 접근이 가능한지 체크
 	static boolean isConnected(List<Integer> group, boolean isGroupA) {
+		// bfs로 체크
 		Queue<Integer> q = new LinkedList<>();
 		boolean[] isVisit = new boolean[N+1];
 		
