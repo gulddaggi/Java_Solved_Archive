f1a5368340b80493413e26320c7ed53284f9ec11
SSAFY <SSAFY@DESKTOP-3HB59PA>
2025-02-26 10:03:35 +0900

    브론즈2

--- problem/src/problem/Problem.java
+++ problem/src/problem/Problem.java
@@ -1,17 +1,44 @@
 package problem;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+/*
+ 문제 : 1977(완전제곱수)
+ 시간 : 172ms
+ 풀이 : 입력 범위를 순회하며 완전제곱수를 판별. 완전제곱수일 경우 가산, 최소값이 아직 없을 경우 갱신.
+ */
+
+import java.util.Scanner;
 
 public class Problem {
-	public static void main(String[] args) throws IOException {
-		// 빠른 입력 사용.
-//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//		StringTokenizer st = new StringTokenizer(br.readLine());
-//		StringBuilder sb = new StringBuilder();
-//
-//		int N = Integer.parseInt(st.nextToken());
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int M = sc.nextInt();
+		int N = sc.nextInt();
+		
+		int sum = 0;
+		int minVal = -1;
+		
+		// 완전제곱수 판별
+		for (int i = M; i <= N; i++) {
+			double root = Math.sqrt(i);
+			
+			// 소수점 아래 값이 존재하지 않으면 완전제곱수
+			if (root % 1 == 0.0) {
+				sum += i;
+				
+				// 최소값이 아직 없을 경우 갱신
+				if (minVal == -1) {
+					minVal = i;
+				}
+			}
+		}
+		
+		if (sum == 0) {
+			System.out.println(-1);
+		}
+		else {
+			System.out.println(sum);
+			System.out.println(minVal);
+		}
 	}
 }
