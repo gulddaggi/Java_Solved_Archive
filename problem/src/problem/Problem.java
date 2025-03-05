89a5f1558a522f030ae1dd1a7dd28fd57260c732
SSAFY <SSAFY@DESKTOP-3HB59PA>
2025-03-05 16:08:12 +0900

    D3

--- problem/src/problem/Problem.java
+++ problem/src/problem/Problem.java
@@ -1,17 +1,49 @@
 package problem;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.Scanner;
 
 public class Problem {
-	public static void main(String[] args) throws IOException {
-		// 빠른 입력 사용.
-//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//		StringTokenizer st = new StringTokenizer(br.readLine());
-//		StringBuilder sb = new StringBuilder();
-//
-//		int N = Integer.parseInt(st.nextToken());
+	static int N;
+	static int K;
+	static int[] arr;
+	static int ans;
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int T = sc.nextInt();
+		
+		for (int testCase = 1; testCase <= T; testCase++) {
+			N = sc.nextInt();
+			K = sc.nextInt();
+			
+			arr = new int[N];
+			int total = 0;
+			for (int i = 0; i < arr.length; i++) {
+				arr[i] = sc.nextInt();
+				total += arr[i];
+			}
+			
+			ans = 0;
+			
+			if (total >= K) func(0, 0);
+			
+			System.out.println("#" + testCase + " " + ans);
+		}
+	}
+	
+	static void func(int idx, int val) {
+        if (val > K) return;
+		
+        if (val == K) {
+        	ans++;
+        	return;
+        }
+		
+        if(idx >= N) return;
+        
+        func(idx + 1, val + arr[idx]);
+        
+        func(idx + 1, val);
 	}
 }
