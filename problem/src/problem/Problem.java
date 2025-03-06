aa15b560d1ef93f06988c1bfdf08f8df187cb383
SSAFY <SSAFY@DESKTOP-3HB59PA>
2025-03-06 13:51:36 +0900

    D2(병합정렬)

--- problem/src/problem/Problem.java
+++ problem/src/problem/Problem.java
@@ -1,17 +1,60 @@
 package problem;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.Scanner;
 
 public class Problem {
-	public static void main(String[] args) throws IOException {
-		// 빠른 입력 사용.
-//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//		StringTokenizer st = new StringTokenizer(br.readLine());
-//		StringBuilder sb = new StringBuilder();
-//
-//		int N = Integer.parseInt(st.nextToken());
+	static int[] A = new int[1000000];
+	static int N = A.length;
+	static int[] temp = new int[N];
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		for (int i = 0; i < N; i++) {
+			A[i] = sc.nextInt();
+		}
+		
+		mergeSort(0, N-1);
+		System.out.println(A[500000]);
+	}
+	
+	static void mergeSort(int start, int end) {
+		if (start < end) {
+			int mid = (start+end) / 2;
+			mergeSort(start, mid);
+			mergeSort(mid+1, end);
+			merge(start, mid, end);
+		}
+	}
+	
+	static void merge(int start, int mid, int end) {
+		int L = start;
+		int R = mid + 1;
+		
+		int idx = start;
+		
+		while (L <= mid && R <= end) {
+			if (A[L] <= A[R]) {
+				temp[idx++] = A[L++];
+			}
+			else {
+				temp[idx++] = A[R++];
+			}
+		}
+		
+        if (L <= mid) {
+        	for (int i = L; i <= mid; i++) {
+            	temp[idx++] = A[i];
+            }
+        }
+        else {
+        	for (int i = R; i <= end; i++) {
+            	temp[idx++] = A[i];
+            }
+        }
+		
+		for (int i = start; i <= end; i++) {
+        	A[i] = temp[i];
+        }
 	}
 }
