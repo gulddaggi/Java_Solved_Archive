b6971c5f61af9a1cd740da1c5d8a8af91f5fa5e5
SSAFY <SSAFY@DESKTOP-3HB59PA>
2025-03-07 10:51:30 +0900

    D3

--- problem/src/problem/Problem.java
+++ problem/src/problem/Problem.java
@@ -1,17 +1,54 @@
 package problem;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.Scanner;
 
 public class Problem {
-	public static void main(String[] args) throws IOException {
-		// 빠른 입력 사용.
-//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//		StringTokenizer st = new StringTokenizer(br.readLine());
-//		StringBuilder sb = new StringBuilder();
-//
-//		int N = Integer.parseInt(st.nextToken());
+	static boolean[] visitCol;
+	static boolean[] visitRUCross;
+	static boolean[] visitRDCross;
+	static int N;
+	static int ans;
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int T = sc.nextInt();
+		
+		for (int testCase = 1; testCase <= T; testCase++) {
+			N = sc.nextInt();
+			
+			visitCol = new boolean[N];
+			visitRUCross = new boolean[2*N];
+			visitRDCross = new boolean[2*N];
+			
+			ans = 0;
+			
+			func(0);
+			
+			System.out.println("#" + testCase + " " + ans);
+		}
+	}
+	
+	static void func(int row) {
+		if (row == N) {
+			++ans;
+			return;
+		}
+		
+		for (int col = 0; col < N; col++) {
+			if(visitCol[col] || visitRUCross[row + col + 1] || visitRDCross[row - col + N])
+				continue;
+			
+			visitCol[col] = true;
+			visitRUCross[row + col + 1] = true;
+			visitRDCross[row - col + N] = true;
+			
+			func(row + 1);
+			
+			visitCol[col] = false;
+			visitRUCross[row + col + 1] = false;
+			visitRDCross[row - col + N] = false;
+
+		}
 	}
 }
