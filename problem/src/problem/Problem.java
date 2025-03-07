aa10eb5321aed716f0c5eaeff47b9d170df90818
SSAFY <SSAFY@DESKTOP-3HB59PA>
2025-03-07 13:58:49 +0900

    D3

--- problem/src/problem/Problem.java
+++ problem/src/problem/Problem.java
@@ -1,17 +1,66 @@
 package problem;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.Scanner;
 
 public class Problem {
-	public static void main(String[] args) throws IOException {
-		// 빠른 입력 사용.
-//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//		StringTokenizer st = new StringTokenizer(br.readLine());
-//		StringBuilder sb = new StringBuilder();
-//
-//		int N = Integer.parseInt(st.nextToken());
+	static int[] kyCards;
+	static int[] iyCards;
+	static boolean[] handed;
+	static int win;
+	static int lose;
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int T = sc.nextInt();
+		
+		for (int testCase = 1; testCase <= T; testCase++) {
+			kyCards = new int[9];
+			iyCards = new int[9];
+			
+			int[] totalCards = new int[19];
+			
+			for (int i = 0; i < kyCards.length; i++) {
+				kyCards[i] = sc.nextInt();
+				
+				++totalCards[kyCards[i]];
+			}
+			
+			int idx = 0;
+			for (int i = 1; i <= 18; i++) {
+				if (totalCards[i] == 0) {
+					iyCards[idx++] = i;
+				}
+			}
+			
+			handed = new boolean[9];
+			win = 0;
+			lose = 0;
+			
+			func(0, 0, 0);
+			
+			System.out.println("#" + testCase + " " + win  + " " + lose);
+		}
+	}
+	
+	static void func(int count, int kyVal, int iyVal) {
+		if (count == 9) {
+			if (kyVal > iyVal) ++win;
+			else if (kyVal < iyVal) ++lose;
+			
+			return;
+		}
+		
+		for (int i = 0; i < kyCards.length; i++) {
+			if (handed[i]) continue;
+			
+			handed[i] = true;
+			
+			int sum = kyCards[count] + iyCards[i];
+			if (kyCards[count] > iyCards[i]) func(count + 1, kyVal + sum, iyVal);
+			else if (kyCards[count] < iyCards[i]) func(count + 1, kyVal, iyVal + sum);
+						
+			handed[i] = false;
+		}
 	}
 }
