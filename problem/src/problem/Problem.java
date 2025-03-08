6b6fbe83d10bf74f040bc390c21dc41a58125e09
gulddaggi <81425764+gulddaggi@users.noreply.github.com>
2025-03-09 02:54:59 +0900

    실버1

--- problem/src/problem/Problem.java
+++ problem/src/problem/Problem.java
@@ -3,10 +3,11 @@ package problem;
 /*
  문제 : 11057(오르막 수)
  시간 : 172ms
- 풀이 : 자릿수가 증가할 때마다 발생하는 오르막 수에 해당하지 않는 수를 이전 개수에서 빼는 과정을 반복하여 개수를 구한다.
+ 풀이 : 이전 자릿수 에서 가장 큰 자릿수(N=1일 경우 일의 자리, N=2일 경우 십의 자리)가 9~0일 때의 오르막 수의 개수를 누적하여 값을 2차원 배열에 저장.
+ 최종 결과는 배열에서 N행의 값을 모두 더하여 도출.
  
- ※ 본 풀이처럼 이전 값에서 규칙적인 값을 빼는 경우, 나머지 연산 과정으로 인해 값 누적 시 음수값이 갱신될 수 있음.
- 이를 해결하기 위해 빼는 수에도 나머지연산 수(MOD) 만큼 값을 더해주어야 정상적인 결과가 출력됨.
+ ※ 기존 풀이는 이전 자릿수의 개수로부터 값을 빼서 계산을 수행.
+ 해당 풀이 링크 : https://github.com/gulddaggi/Java_Solved_Archive/blob/1569aa0c70618768295f2c3efd0c122089a10f9f/problem/src/problem/Problem.java
  */
 
 import java.util.Scanner;
@@ -18,33 +19,25 @@ public class Problem {
 		int N = sc.nextInt();
         int MOD = 10007;  // 모듈러 값 상수로 정의
 		
-        // 0번 인덱스 : 최종 값
-        // 1~10번 인덱스 : 빼는 값
-		long[][] dp = new long[1001][11];
+		int[][] dp = new int[N+1][10];
 		
-		dp[1][0] = 10;
-		dp[2][0] = 55;
+		// 초기값
+		for (int i = 0; i < 10; i++) dp[1][i] = 1;
 		
-		for (int i = 1; i <= 10; i++) dp[1][i] = i;
-		
-		for (int i = 3; i <= N; i++) {
-			dp[i][0] = dp[i - 1][0];
-			
-			long val = 0;
-			for (int j = 1; j <= 10; j++) {
-				// 빼는 값 계산
-				if (j == 1) dp[i-1][j] = (val + dp[i-2][0]) % MOD;
-				else dp[i-1][j] = (val + dp[i-2][0] - dp[i-2][j-1] + MOD) % MOD; // 음수 방지 위해 +MOD 추가
-
-				val = dp[i-1][j];
-				
-				// 값 누적
-				dp[i][0] = (dp[i][0] + dp[i-1][0] - dp[i-1][j] + MOD) % MOD; // 음수 방지 위해 +MOD 추가
+		for (int i = 2; i <= N; i++) {
+			for (int j = 0; j < 10; j++) {
+				for (int k = 0; k <= j; k++) {
+                    dp[i][j] = (dp[i][j] + dp[i - 1][k]) % MOD;
+				}
 			}
 		}
 		
-		dp[N][0] = dp[N][0] % MOD;
+		// N행의 모든 값을 더하면 최종 결과
+		int ans = 0;
+		for (int i = 0; i < 10; i++) {
+			ans = (ans + dp[N][i]) % MOD;
+		}
 		
-		System.out.println(dp[N][0] % 10007);
+		System.out.println(ans);
 	}
 }
