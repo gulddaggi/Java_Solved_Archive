0de1ccb07c94a6b46ce098ec1a7e97d2a308a4e5
gulddaggi <81425764+gulddaggi@users.noreply.github.com>
2025-03-09 03:15:12 +0900

    실버3

--- problem/src/problem/Problem.java
+++ problem/src/problem/Problem.java
@@ -1,17 +1,28 @@
 package problem;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+/*
+ 문제 : 1904(01타일)
+ 시간 : 188ms
+ 풀이 : 피보나치 수열과 같은 규칙성을 가지므로 이를 활용하여 입력 인덱스까지 값을 계산하여 출력.
+ */
+
+import java.util.Scanner;
 
 public class Problem {
-	public static void main(String[] args) throws IOException {
-		// 빠른 입력 사용.
-//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//		StringTokenizer st = new StringTokenizer(br.readLine());
-//		StringBuilder sb = new StringBuilder();
-//
-//		int N = Integer.parseInt(st.nextToken());
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		
+		int[] arr = new int[1000001];
+		
+		arr[1] = 1;
+		arr[2] = 2;
+		
+		for (int i = 3; i <= N; i++) {
+			arr[i] = (arr[i-1] + arr[i-2]) % 15746;
+		}
+		
+		System.out.println(arr[N]);
 	}
 }
