93db98bbb81737f237d9efa515809893f61b3aa8
gulddaggi <81425764+gulddaggi@users.noreply.github.com>
2025-03-08 20:36:25 +0900

    실버3

--- problem/src/problem/Problem.java
+++ problem/src/problem/Problem.java
@@ -1,5 +1,12 @@
 package problem;
 
+/*
+ 문제 : 2193(이친수)
+ 시간 : 176ms
+ 풀이 : N자리 이친수의 개수는 아래 식과 같은 규칙을 가지므로 이를 활용해 개수를 구한다.
+ 이친수 N자리 이친수 개수 = N-1자리 이친수 개수 + N-2 이친수 개수
+ */
+
 import java.util.Scanner;
 
 public class Problem {
@@ -8,10 +15,14 @@ public class Problem {
 		
 		int N = sc.nextInt();
 		
+		// 입력 범위만큼 배열 사용
 		long[] dp = new long[91];
+		
+		// 초기값
 		dp[1] = 1;
 		dp[2] = 1;
 		
+		// 규칙에 맞게 계산 및 저장
 		for (int i = 3; i <= N; i++) {
 			dp[i] = dp[i - 1] + dp[i - 2];
 		}
