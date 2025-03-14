42b016afb014f1955088890081d723bbd8e98993
gulddaggi <81425764+gulddaggi@users.noreply.github.com>
2025-03-14 22:31:53 +0900

    실버 5

--- problem/src/problem/Problem.java
+++ problem/src/problem/Problem.java
@@ -1,17 +1,40 @@
 package problem;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+/*
+ 문제 : 18511(큰 수 구성하기)
+ 시간 : 172ms
+ 풀이 : 구성된 수가 N보다 커질때까지 재귀를 수행하며 최댓값을 갱신.
+ */
+
+import java.util.Scanner;
 
 public class Problem {
-	public static void main(String[] args) throws IOException {
-		// 빠른 입력 사용.
-//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//		StringTokenizer st = new StringTokenizer(br.readLine());
-//		StringBuilder sb = new StringBuilder();
-//
-//		int N = Integer.parseInt(st.nextToken());
+	static int N;
+	static int ans;
+	static int[] arr;
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		N = sc.nextInt();
+		int K = sc.nextInt();
+		
+		arr = new int[K];
+		for (int i = 0; i < K; i++) 
+			arr[i] = sc.nextInt();
+		
+		ans = 0;
+		find(0);
+		
+		System.out.println(ans);
+	}
+	
+	static void find(int val) {
+		if (val > N) return;
+		
+		ans = Math.max(ans, val);
+		
+		for (int i = 0; i < arr.length; i++) 
+			find(val *10 + arr[i]);
 	}
 }
