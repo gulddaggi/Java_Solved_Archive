0f4133fc1d3fbc968ca6c013a3d499b2a12264cc
gulddaggi <81425764+gulddaggi@users.noreply.github.com>
2025-03-16 13:02:21 +0900

    실버2

--- problem/src/problem/Problem.java
+++ problem/src/problem/Problem.java
@@ -12,7 +12,6 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.Scanner;
 import java.util.StringTokenizer;
 
 public class Problem {
@@ -22,7 +21,6 @@ public class Problem {
 	static boolean[][][] check;
 	
 	public static void main(String[] args) throws IOException {
-		Scanner sc = new Scanner(System.in);
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringTokenizer st = new StringTokenizer(br.readLine());
