96697c74906ef704f454efd36fba97880a629a6b
SSAFY <SSAFY@DESKTOP-3HB59PA>
2025-03-18 17:53:22 +0900

    임시

--- problem/src/problem/Problem.java
+++ problem/src/problem/Problem.java
@@ -1,18 +1,83 @@
 package problem;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.Scanner;
+import java.util.Stack;
 
 public class Problem {
-	public static void main(String[] args) throws IOException {
-		// 빠른 입력 사용.
-//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//		StringTokenizer st = new StringTokenizer(br.readLine());
-//		StringBuilder sb = new StringBuilder();
-//
-//		int N = Integer.parseInt(st.nextToken());
+	static int ans;
+	static String S;
+	static Stack<Character> st;
+	static int idx;
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 		
+		S = sc.next();
+		ans = 0;
+		idx = 0;
+		st = new Stack<>();
+		
+		ans += unZip(0);
+		System.out.println(ans);
+	}
+	
+	static int unZip(int k) {	
+		int count = 0;
+		
+		while (idx < S.length()) {
+			char cur = S.charAt(idx);
+			
+			// 여는 괄호
+			if (cur == '(') {
+				++count;
+				
+				// k가 숫자인경우
+				if (idx - 1 >= 0 && Character.isDigit(S.charAt(idx-1))) {
+					// 스택에 추가
+					st.add(cur);
+					// k 저장
+					int tmp = S.charAt(idx-1) - '0';
+					
+					System.out.println("k : " + tmp);
+					
+					// 인덱스 이동
+					++idx;
+					// 압축 해제 수행
+					count += unZip(tmp);
+					
+					System.out.println("count : " + count);
+				}
+			}
+			// 닫는 괄호
+			else if (cur == ')') {
+				// 여는 괄호가 존재하면 압축 풀기
+				if (!st.isEmpty()) {
+					// 괄호 쌍 제거
+					st.pop();
+					
+					System.out.println("k : " + k + ", num : " + (k * count - 2));
+					
+					System.out.println("ans : " + ans);
+					
+					// k와 여는괄호 계산 후 반환
+					return (k * count - 2);
+				}
+				// 여는 괄호가 없으면 단일 카운팅
+				else {
+					++count;
+				}
+			}
+			// 숫자
+			else {
+				// 카운팅
+				++count;
+			}
+			
+			// 인덱스 이동
+			++idx;
+		}
+		
+		// 압축 아닌경우
+		return count;
 	}
 }
