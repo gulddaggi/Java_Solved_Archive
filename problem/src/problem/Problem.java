7df0fbe0179a821d44805e4928c47d5d3be04d45
SSAFY <SSAFY@DESKTOP-3HB59PA>
2025-03-24 14:26:00 +0900

    D5

--- problem/src/problem/Problem.java
+++ problem/src/problem/Problem.java
@@ -1,18 +1,62 @@
 package problem;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class Problem {
-	public static void main(String[] args) throws IOException {
-		// 빠른 입력 사용.
-//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//		StringTokenizer st = new StringTokenizer(br.readLine());
-//		StringBuilder sb = new StringBuilder();
-//
-//		int N = Integer.parseInt(st.nextToken());
+	static List<Integer>[] graph;
+	static boolean[] visit;
+	static int ans;
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 		
+		int T = sc.nextInt();
+		for (int testCase = 1; testCase <= T; testCase++) {
+			int N = sc.nextInt();
+			int M = sc.nextInt();
+			
+			graph = new ArrayList[N+1];
+			
+			for (int idx = 1; idx <= N; idx++) {
+				graph[idx] = new ArrayList<>();
+			}
+			
+			for (int j = 0; j < M; j++) {
+				int a = sc.nextInt();
+				int b = sc.nextInt();
+				
+				graph[a].add(b);
+				graph[b].add(a);
+			}
+			
+			ans = 0;
+			visit = new boolean[N+1];
+			
+			dfs(1, 0);
+			
+			
+			System.out.println("#" + testCase + " " + (ans-1));
+		}
+		
+	}
+	
+	static void dfs(int v, int depth) {
+		if (depth == 3) {
+			return;
+		}
+		
+		if (!visit[v]) {
+			++ans;
+		}
+		
+		visit[v] = true;
+		
+		for (int nxt : graph[v]) {
+			if (nxt != 1) {
+				dfs(nxt, depth + 1);
+			}
+		}
 	}
 }
