be9e4f80ae059bddfc2f2394cc76fc8e03a0fa10
gulddaggi <81425764+gulddaggi@users.noreply.github.com>
2025-04-02 01:19:24 +0900

    브론즈2

--- problem/src/problem/Problem.java
+++ problem/src/problem/Problem.java
@@ -7,11 +7,31 @@ import java.util.StringTokenizer;
 
 public class Problem {
 	public static void main(String[] args) throws IOException {
-		// 빠른 입력 사용.
-//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//		StringTokenizer st = new StringTokenizer(br.readLine());
-//		StringBuilder sb = new StringBuilder();
-//
-//		int N = Integer.parseInt(st.nextToken());
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringBuilder sb = new StringBuilder();
+
+		int T = Integer.parseInt(st.nextToken());
+		for (int i = 0; i < T; i++) {
+			st = new StringTokenizer(br.readLine());
+			
+			double val = Double.parseDouble(st.nextToken());
+			
+			while (st.hasMoreTokens()) {
+				String s = st.nextToken();
+				
+				if (s.equals("@")) {
+					val *= 3;
+				}
+				else if (s.equals("%")) {
+					val += 5;
+				}
+				else {
+					val -= 7;
+				}
+			}
+			
+			System.out.printf("%.2f\n", val);
+		}
 	}
 }
