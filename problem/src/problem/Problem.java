47da467c040789ce0c0285dbbefdf9bfa74581b4
gulddaggi <81425764+gulddaggi@users.noreply.github.com>
2025-04-09 23:58:12 +0900

    브론즈2

--- problem/src/problem/Problem.java
+++ problem/src/problem/Problem.java
@@ -8,11 +8,31 @@ import java.util.StringTokenizer;
 public class Problem {
 	public static void main(String[] args) throws IOException {
 		// 빠른 입력 사용.
-//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//		StringTokenizer st = new StringTokenizer(br.readLine());
-//		StringBuilder sb = new StringBuilder();
-//
-//		int N = Integer.parseInt(st.nextToken());
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringBuilder sb = new StringBuilder();
+
+		int n = Integer.parseInt(st.nextToken());
+		
+		int[] arr = new int[n];
+		
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		long ans = 0;
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = 0; j < arr.length; j++) {
+				if (i == j) {
+					continue;
+				}
+				
+				ans += Math.abs(arr[i] - arr[j]);
+			}
+		}
+		
+		System.out.println(ans);
 		
 	}
 }
