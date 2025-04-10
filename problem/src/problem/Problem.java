5233fd667eee0d9ee9a1a693ff62bfc1d5b208ef
gulddaggi <81425764+gulddaggi@users.noreply.github.com>
2025-04-11 00:09:46 +0900

    브론즈1

--- problem/src/problem/Problem.java
+++ problem/src/problem/Problem.java
@@ -1,18 +1,35 @@
-package problem;
+import java.io.*;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // BufferedReader 사용
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-public class Problem {
-	public static void main(String[] args) throws IOException {
-		// 빠른 입력 사용.
-//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//		StringTokenizer st = new StringTokenizer(br.readLine());
-//		StringBuilder sb = new StringBuilder();
-//
-//		int N = Integer.parseInt(st.nextToken());
-		
-	}
+        // 키 입력
+        int key = Integer.parseInt(br.readLine());
+
+        // 암호화된 문장 입력
+        String line = br.readLine();
+
+        StringBuilder decrypted = new StringBuilder();
+
+        for (int i = 0; i < line.length(); i++) {
+            char ch = line.charAt(i);
+
+            if (ch >= 'a' && ch <= 'z') {
+                // 복호화 수행
+                int shift = (ch - 'a' - key + 26) % 26;
+                decrypted.append((char) ('a' + shift));
+
+                // 키 증가 및 25 넘어가면 1로 순환
+                key++;
+                if (key > 25) key = 1;
+            } else {
+                // 문자 그대로 추가
+                decrypted.append(ch);
+            }
+        }
+
+        System.out.println(decrypted.toString());
+    }
 }
