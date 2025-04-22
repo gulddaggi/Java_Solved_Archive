f215a0396f7c1ea40c36958eddc81d3715d451e2
gulddaggi <81425764+gulddaggi@users.noreply.github.com>
2025-04-22 11:34:32 +0900

    실버 3

--- problem/src/problem/Problem.java
+++ problem/src/problem/Problem.java
@@ -3,16 +3,64 @@ package problem;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class Problem {
 	public static void main(String[] args) throws IOException {
-		// 빠른 입력 사용.
-//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//		StringTokenizer st = new StringTokenizer(br.readLine());
-//		StringBuilder sb = new StringBuilder();
-//
-//		int N = Integer.parseInt(st.nextToken());
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		
+		int[] scores = new int[N];
+		st = new StringTokenizer(br.readLine());
+		
+		for (int i = 0; i < scores.length; i++) {
+			scores[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		int[] limits = new int[N-M+1];
+		st = new StringTokenizer(br.readLine());
+		
+		for (int i = 0; i < limits.length; i++) {
+			limits[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		PriorityQueue<Integer> pq = new PriorityQueue<>();
+		
+		int count = N;
+		int idx = 0;
+		int round = 0;
+		
+		Stack<Integer> tmp = new Stack<>();
+		
+		while (count != 0) {
+			int addCount = M - pq.size();
+			
+			for (int i = 0; i < addCount; i++) {
+				pq.add(scores[idx++]);
+			}
+			
+			for (int i = 0; i < limits[round]; i++) {
+				tmp.add(pq.poll());
+			}
+			
+			tmp.pop();
+			
+			while (!tmp.isEmpty()) {
+				pq.add(tmp.pop());
+			}
+			
+			count -= addCount;
+			round++;
+		}
+		
+		while (!pq.isEmpty()) {
+			System.out.println(pq.poll());
+		}
 		
 	}
 }
