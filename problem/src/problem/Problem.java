40683e8d40a606a9e28e3465ec3fdca4a6df2e8b
gulddaggi <81425764+gulddaggi@users.noreply.github.com>
2025-04-23 17:38:25 +0900

    브론즈 3

--- problem/src/problem/Problem.java
+++ problem/src/problem/Problem.java
@@ -1,18 +1,37 @@
 package problem;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class Problem {
 	public static void main(String[] args) throws IOException {
-		// 빠른 입력 사용.
-//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//		StringTokenizer st = new StringTokenizer(br.readLine());
-//		StringBuilder sb = new StringBuilder();
-//
-//		int N = Integer.parseInt(st.nextToken());
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int T = Integer.parseInt(st.nextToken());
+		int[] changes = {25, 10, 5, 1};
 		
+		while (T-- != 0) {
+			StringBuilder sb = new StringBuilder();
+			int C = Integer.parseInt(br.readLine());
+			
+			int[] answer = {0, 0, 0, 0};
+			for (int i = 0; i < changes.length; i++) {
+				answer[i] = C / changes[i];
+				C -= (answer[i] * changes[i]);
+				sb.append(answer[i] + " ");
+			}
+			
+			
+			bw.write(sb.toString() + "\n");
+			bw.flush();
+		}
+		
+		bw.close();
 	}
 }
